c212a6b35ac38f9f93184c98423630910c2c2845
dipak <ramoliyadipak333@gmail.com>
2021-06-05 11:11:34 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import 'model.dart';
 
@@ -20,25 +20,25 @@ class _MyCrudeState extends State<MyCrude> {
 
 
   List<ls> list = [
-    ls(lastname: 'ramoiya', firstname: 'dipak', userid: '1'),
-    ls(lastname: 'jani', firstname: 'yash', userid: '2'),
-    ls(lastname: 'vaghasiya', firstname: 'parth', userid: '3'),
-    ls(lastname: 'vaghasiya', firstname: 'raj', userid: '3'),
-    ls(lastname: 'kasundra', firstname: 'manthan', userid: '5'),
-    ls(lastname: 'chauhan', firstname: 'dharmik', userid: '6'),
-    ls(lastname: 'bhut', firstname: 'jigar', userid: '7'),
-    ls(lastname: 'bharodiya', firstname: 'dhaval', userid: '8'),
-    ls(lastname: 'bharodiya', firstname: 'pratik', userid: '9'),
-    ls(lastname: 'sarola', firstname: 'brijesh', userid: '10'),
-    ls(lastname: 'anghan', firstname: 'brijesh', userid: '12'),
-    ls(lastname: 'anghan', firstname: 'bhavesh', userid: '13'),
-    ls(lastname: 'anghan', firstname: 'vishal', userid: '14'),
-    ls(lastname: 'dobariya', firstname: 'bhargav', userid: '15'),
-    ls(lastname: 'sarola', firstname: 'arun', userid: '16'),
-    ls(lastname: 'dobariya', firstname: 'utsav', userid: '17'),
-    ls(lastname: 'radadiya', firstname: 'abhishek', userid: '18'),
-    ls(lastname: 'dholiya', firstname: 'bhautik', userid: '19'),
-    ls(lastname: 'yadav', firstname: 'niraj', userid: '20'),
+    ls(lastname: 'ramoiya', firstname: 'dipak', userid: '1',dates: 1037385000000 ),
+    ls(lastname: 'jani', firstname: 'yash', userid: '2',dates: 1008095400000),
+    ls(lastname: 'vaghasiya', firstname: 'parth', userid: '3',dates: 1323628200000),
+    ls(lastname: 'vaghasiya', firstname: 'raj', userid: '3',dates: 1037345000000),
+    ls(lastname: 'kasundra', firstname: 'manthan', userid: '5',dates: 1437385000000),
+    ls(lastname: 'chauhan', firstname: 'dharmik', userid: '6',dates: 1037386000000),
+    ls(lastname: 'bhut', firstname: 'jigar', userid: '7',dates: 1057385000000),
+    ls(lastname: 'bharodiya', firstname: 'dhaval', userid: '8',dates: 1037385000000),
+    ls(lastname: 'bharodiya', firstname: 'pratik', userid: '9',dates: 1037385000000),
+    ls(lastname: 'sarola', firstname: 'brijesh', userid: '10',dates: 1037385000000),
+    ls(lastname: 'anghan', firstname: 'brijesh', userid: '12',dates: 1037385000000),
+    ls(lastname: 'anghan', firstname: 'bhavesh', userid: '13',dates: 1037385000000),
+    ls(lastname: 'anghan', firstname: 'vishal', userid: '14',dates: 1323628200000),
+    ls(lastname: 'dobariya', firstname: 'bhargav', userid: '15',dates: 1101925800000),
+    ls(lastname: 'sarola', firstname: 'arun', userid: '16',dates: 976559400000),
+    ls(lastname: 'dobariya', firstname: 'utsav', userid: '17',dates: 1037385000000),
+    ls(lastname: 'radadiya', firstname: 'abhishek', userid: '18',dates: 1037385000000),
+    ls(lastname: 'dholiya', firstname: 'bhautik', userid: '19',dates: 1137385000000),
+    ls(lastname: 'yadav', firstname: 'niraj', userid: '20',dates: 1323627200000),
   ];
 
   bool isrev = false;
@@ -51,10 +51,10 @@ class _MyCrudeState extends State<MyCrude> {
         leading: IconButton(onPressed: () {
           setState(() {
               if(isrev){
-                list.sort((a,b) => b.firstname.compareTo(a.firstname));
+                list.sort((a,b) => b.dates.compareTo(a.dates));
               }
               else {
-                list.sort((a,b) => a.firstname.compareTo(b.firstname));
+                list.sort((a,b) => a.dates.compareTo(b.dates));
               }
              isrev = !isrev;
           });
@@ -73,8 +73,9 @@ class _MyCrudeState extends State<MyCrude> {
         itemBuilder: (BuildContext context,index){
           return Card(
             elevation: 10,
-            child: Text( "  Id:- ${list[index].userid}   \n  First Name:-${list[index].firstname}     \n  Lastname:-${list[index].lastname}",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500),),
+            child: Text( "  Id:- ${list[index].userid}   \n  First Name:-${list[index].firstname}     \n  Lastname:-${list[index].lastname} \n Birthday:-${DateFormat.yMMMd().format(DateTime.fromMillisecondsSinceEpoch(list[index].dates)) }",style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500),),
           );
         });
   }
+
 }
--- lib/model.dart
+++ lib/model.dart
@@ -1,14 +1,24 @@
 
+import 'dart:core';
+
+import 'package:intl/intl.dart';
+
 class ls{
   ls({
    required this.userid,
     required this.firstname,
     required this.lastname,
+    required this.dates,
+
   });
 
   String userid;
   String firstname;
   String lastname;
+  int dates;
+
+
+
 
 
 }
